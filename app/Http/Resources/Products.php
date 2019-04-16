d25153a935d138b36590142e30090bbd830658bc
snakewar <mayrcon_marlon@hotmail.com>
2019-04-16 13:22:13 -0300

    Fazer API

---
+++ app/Http/Resources/Products.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Products extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'product_name' => $this->product_name,
+            'product_description' => $this->product_description,
+            'product_price' => $this->product_price,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
