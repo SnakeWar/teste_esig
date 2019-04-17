5e4dfafaacba876a4021bd473479f5acff4b9ff6
snakewar <mayrcon_marlon@hotmail.com>
2019-04-17 16:07:37 -0300

    Aplicar Teste

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,16 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Products;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function testApplication()
+    {
+        $response = $this->call('GET', '/products');
+
+        $this->assertEquals(200, $response->status());
+    }
 }
